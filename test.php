e79e0f570767ec4804d8d251817342b4d8d5c11f
glovebx <glovebx@glovetekiMacBook-Pro.local>
2014-11-27 02:09:07 +0800

    加了个测试文件

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
 
-include_once('sqslog.php');
+//include_once('sqslog.php');
 
-echo sqslog::log('test', 'test_value');
+//echo sqslog::log('test', 'test_value');
+
+include_once('kcsqs_client.php');
+$kcsqs = new kcsqs();
+
+$kcsqs->put('test', '你好test_value');
+echo 'result='.$kcsqs->get('wxRev');
